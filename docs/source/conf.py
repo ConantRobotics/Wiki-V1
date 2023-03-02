b82821f13b40ecc13eefcc7555447a28104ba558
ConantRobotics <126821905+ConantRobotics@users.noreply.github.com>
2023-03-02 14:50:18 -0600

    conant author

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Conant Robotics'
+copyright = '2023, Conant Robotics'
+author = 'Conant Robotics Club 2023'
 
 release = '0.1'
 version = '0.1.0'
